5b645abbfe310fd3afda69074972922a4703f6fe
Aditya Dixit <iittop69@gmail.com>
2024-08-13 09:58:15 +0530

    modified metadata and applied font

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+//Function by ShadCn that allow to merge couple of class names together
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
