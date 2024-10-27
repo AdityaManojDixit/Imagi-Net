a5dc84e52de8d1575dffd046e30da56ac005cd39
Aditya Dixit <iittop69@gmail.com>
2024-10-27 19:36:05 +0530

    Refactor image paths in Collection component and image model

--- src/components/ui/shared/Collection.tsx
+++ src/components/ui/shared/Collection.tsx
@@ -56,8 +56,8 @@ export const Collection = ({
 
       {images.length > 0 ? (
         <ul className="collection-list">
-          {images.map((image) => (
-            <Card image={image} key={image._id} />
+          {images.map((image: IImage) => (
+            <Card image={image} key={image._id as React.Key} />
           ))}
         </ul>
       ) : (
